12c342fc7d50d812bb3924aa10db19157d0da32d
JohnTheArtist <johnr2d20@gmail.com>
2026-09-10 10:02:43 -0400

    commit 1

---
+++ src/main.java
@@ -0,0 +1,37 @@
+public class main {
+
+
+    // initialization
+    // check condition
+    // Increment/exectution
+    // go to step 2
+
+
+
+
+}
+
+public static void main(String[] args){
+//    for(int i =0; i <= 10; i++){
+//
+//        System.out.println(i);
+//
+//    }
+//    System.out.println("closed loop");
+
+    for(int j =1, u = 5; j <= 5; j++){
+
+        for(int i =0; i <= u; i++){
+            System.out.print(i + " ");
+
+        }
+
+        System.out.println();
+        u--;
+
+
+    }
+//    System.out.println("closed loop");
+
+
+}
